36f740d71895bd708950cb79c2dee7aa9c049a41
Maxwell Zilvester <zilimax@gmail.com>
2021-01-16 20:06:53 +0000

    Saxophone test pass coomit

--- src/main/java/Instrument/Saxophone.java
+++ src/main/java/Instrument/Saxophone.java
@@ -1,4 +1,18 @@
 package Instrument;
 
-public class Saxophone {
+import MusicalIntrument.Instrument;
+
+
+public class Saxophone extends Instrument {
+
+    private int keys;
+
+    public Saxophone(String name, String rawMaterials, String colour, String type, String description, double buyPrice, double sellPrice, int keys) {
+        super(name, rawMaterials, colour, type, description, buyPrice, sellPrice);
+        this.keys = keys;
+    }
+
+    public int getKeys() {
+        return keys;
+    }
 }
--- src/test/java/SaxophoneTest.java
+++ src/test/java/SaxophoneTest.java
@@ -1,2 +1,23 @@
+import Instrument.Piano;
+import Instrument.Saxophone;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SaxophoneTest {
+
+    Saxophone saxophone;
+    Saxophone saxophone1;
+
+    @Before
+    public void before(){
+        saxophone = new Saxophone("Sopranino Sax", "Brass", "Gold", "Sopranino", "provides the highest pitch", 105, 175, 8);
+        saxophone1 = new Saxophone("Alto Sax", "Copper", "Silver", "Baritone", "provides a deep a powerful sound", 225, 350, 6);
+    }
+
+    @Test
+    public void saxophoneHasKeys(){
+        assertEquals(8, saxophone.getKeys());
+    }
 }
