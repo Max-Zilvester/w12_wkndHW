d017cd6a4dfb501078eef92edb369491f91ac94f
Maxwell Zilvester <zilimax@gmail.com>
2021-01-17 16:45:20 +0000

    Shop created, add/remove test passed

--- src/main/java/MusicShop/Shop.java
+++ src/main/java/MusicShop/Shop.java
@@ -1,4 +1,28 @@
 package MusicShop;
 
+import Behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stocks;
+
+    public Shop(String name){
+        this.name = name;
+        this.stocks = new ArrayList<ISell>();
+    }
+
+    public int stockCount(){
+        return stocks.size();
+    }
+
+    public void addItemsToStocks(ISell item){
+        this.stocks.add(item);
+    }
+
+    public void removeItemsFromStocks(ISell item){
+        this.stocks.remove(item);
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,34 @@
+import Instrument.Guitar;
+import Instrument.Piano;
+import Instrument.Saxophone;
+import MusicShop.Shop;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+    Shop shop;
+    Guitar guitar;
+    Saxophone saxophone;
+    Piano piano;
+
+    @Before
+    public void before(){
+        shop = new Shop("Max's Music");
+        guitar = new Guitar("Fender Stratocaster", "Wire", "Red", "6-String", "Has six strings", 10, 15, 6);
+    }
+
+    @Test
+    public void addNewGuitar(){
+        shop.addItemsToStocks(guitar);
+        assertEquals(1, shop.stockCount());
+    }
+    @Test
+    public void removeGuitar() {
+        shop.removeItemsFromStocks(guitar);
+        assertEquals(0, shop.stockCount());
+    }
+
+
 }
